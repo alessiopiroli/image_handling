97393a31528d296e74d523d6b5a63840c705cb51
alessiopiroli <56607081+alessiopiroli@users.noreply.github.com>
2025-04-08 20:08:15 +0200

    Update collage.py

--- collage.py
+++ collage.py
@@ -1,40 +1,28 @@
 from PIL import Image
 import os
 
-# List of image file paths
-image_paths = ['/Volumes/SANDISK/work_UniSi/renamed_photos_collage/one_level/Image_8.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/one_level/Image_2.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/one_level/Image_10.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/one_level/Image_6.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/one_level/Image_5.JPG',
-               '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/two_levels/Image_18.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/two_levels/Image_16.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/two_levels/Image_13.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/two_levels/Image_14.JPG', '/Volumes/SANDISK/work_UniSi/renamed_photos_collage/two_levels/Image_12.JPG']
-
-# Open all the images
+image_paths = []
 images = [Image.open(image).convert("RGBA") for image in image_paths]  # Convert images to RGBA mode for transparency
-
-# Get the size of the images (assuming all images have the same size)
 image_width, image_height = images[0].size
 
-# Calculate the size of the collage
-collage_width = image_width * 5  # 5 images per row
-collage_height = image_height * 2  # 2 rows
+collage_width = image_width * 5
+collage_height = image_height * 2
 
-# Create a blank canvas with a transparent background (RGBA mode)
-collage = Image.new('RGBA', (collage_width, collage_height), (0, 0, 0, 0))  # (0,0,0,0) for transparency
+collage = Image.new('RGBA', (collage_width, collage_height), (0, 0, 0, 0))
 
-# Paste images onto the collage
 for i, image in enumerate(images):
-    row = i // 5  # Determine the row (0 or 1)
-    col = i % 5   # Determine the column (0 to 4)
+    row = i // 5
+    col = i % 5
     x_offset = col * image_width
     y_offset = row * image_height
-    collage.paste(image, (x_offset, y_offset), image)  # Use the image itself as a mask to preserve transparency
+    collage.paste(image, (x_offset, y_offset), image)
 
-# Define the folder and filename
-save_folder = "/Volumes/SANDISK/work_UniSi/grasping_collage"  # Change to your desired folder
-save_filename = "collage_2.png"  # Change the filename as needed
+save_folder = "folder"
+save_filename = "collage.extension"
 save_path = os.path.join(save_folder, save_filename)
 
-# Ensure the folder exists
 os.makedirs(save_folder, exist_ok=True)
 
-# Save the collage as PNG (keeping transparency)
 collage.save(save_path)
 print(f"Collage saved at: {save_path}")
 collage.show()
